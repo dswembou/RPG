1e88c09a608f4957426b1466797621e5e63a0960
dswembou <56019569+dswembou@users.noreply.github.com>
2021-03-04 20:26:37 +0100

    click to move added

---
+++ Assets/Mover.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ClickToMove();
+    }
+
+    void ClickToMove()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            MoveToCursor();
+            
+        }
+        ;
+
+        
+    }
+
+    void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(ray, out hit);
+        if (hasHit)
+        {
+            GetComponent<NavMeshAgent>().destination = hit.point;
+        }
+        
+    }
+}
