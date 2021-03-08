72e53cf7397cc0de56b109f8e85bed74f164e76e
dswembou <56019569+dswembou@users.noreply.github.com>
2021-03-08 08:57:57 +0100

    update

---
+++ Assets/BaseStats.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseStats : MonoBehaviour
+{
+    [Range(1, 99)] [SerializeField] private int startingLevel = 1;
+    [SerializeField] private CharacterClass characterClass;
+    [SerializeField] private Progression progression = null;
+
+    public float GetHealth()
+    {
+        return progression.GetHealth(characterClass, startingLevel);
+    }
+}
